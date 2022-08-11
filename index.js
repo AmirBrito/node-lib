04bf1ed5d03d5820b91c0e9ac620d5a896e493bb
amirb <amir.sbrito@gmail.com>
2022-08-10 23:15:13 -0300

    feat: add function to handle errors

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import chalk from 'chalk';
 import * as fs from 'fs';
 
+function handleError(error) {
+  throw new Error(chalk.yellow(error))
+}
+
 function getArchive(pathArchive) {
   const encoding = 'utf-8';
-  fs.readFile(pathArchive, encoding, (_, text) => {
+  fs.readFile(pathArchive, encoding, (error, text) => {
+    if (error) handleError(error);
     console.log(chalk.green(text));
   })
 }
