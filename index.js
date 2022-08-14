9d25f13a6313f44ac49f84a3aa8d69ec64822884
amirb <amir.sbrito@gmail.com>
2022-08-14 18:32:07 -0300

    feat: refact function get links, regex function, new struct data object to store links and content

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import chalk from 'chalk';
 import * as fs from 'fs';
 
-const text = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
-
 function getLinks(text) {
   const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-  const extractedLink = text.match(regex);
-  console.log(extractedLink);
+  const linksResult = [];
+  let temp;
+  
+  while(( temp = regex.exec(text)) != null) {
+      linksResult.push({ [temp[1]]: temp[2] });
+  }
+  console.log(linksResult);
 }
-
-getLinks(text);
 
 function handleError(error) {
   throw new Error(chalk.yellow(error))
@@ -19,7 +20,7 @@ async function getArchive(pathArchive) {
   try {
     const encoding = 'utf-8';
     const text =  await fs.promises.readFile(pathArchive, encoding)
-    console.log(chalk.blue(text))
+    getLinks(text);
   } catch (e) {
     handleError(e)
   }
