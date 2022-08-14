f05749da9643cc617c7c0379a6c90f09478d201f
amirb <amir.sbrito@gmail.com>
2022-08-13 23:42:26 -0300

    feat: remove old function

--- index.js
+++ index.js
@@ -14,12 +14,5 @@ function getArchive(pathArchive) {
   .catch((error) => handleError(error))
 
 }
-/* function getArchive(pathArchive) {
-  const encoding = 'utf-8';
-  fs.readFile(pathArchive, encoding, (error, text) => {
-    if (error) handleError(error);
-    console.log(chalk.green(text));
-  })
-} */
 
 getArchive('./archives/textLink.md');
