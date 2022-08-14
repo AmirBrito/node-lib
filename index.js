4e8eeef9922096fcd43c48fce4161ad94f3ec0dd
amirb <amir.sbrito@gmail.com>
2022-08-13 23:41:56 -0300

    feat: refact get archive function to implement asyncronous flow

--- index.js
+++ index.js
@@ -7,10 +7,19 @@ function handleError(error) {
 
 function getArchive(pathArchive) {
   const encoding = 'utf-8';
+
+  fs.promises
+  .readFile(pathArchive, encoding)
+  .then((text) => console.log(chalk.green(text)))
+  .catch((error) => handleError(error))
+
+}
+/* function getArchive(pathArchive) {
+  const encoding = 'utf-8';
   fs.readFile(pathArchive, encoding, (error, text) => {
     if (error) handleError(error);
     console.log(chalk.green(text));
   })
-}
+} */
 
 getArchive('./archives/textLink.md');
