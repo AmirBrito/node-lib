3691a8549011a79233c2e26882edd2438210bfd9
amirb <amir.sbrito@gmail.com>
2022-08-14 18:10:59 -0300

    feat: implement regex function to identify and get link in a text

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import chalk from 'chalk';
 import * as fs from 'fs';
 
+const text = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
+
+function getLinks(text) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const extractedLink = text.match(regex);
+  console.log(extractedLink);
+}
+
+getLinks(text);
+
 function handleError(error) {
   throw new Error(chalk.yellow(error))
 }
